026b1542e7740cc29c8c38056a4418f4aa7433ac
Rinat <rin63@mail.ru>
2022-12-07 13:44:57 +0400

    2 commit

--- app/src/main/java/com/e/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/e/playlistmaker/SearchActivity.kt
@@ -1,11 +1,78 @@
 package com.e.playlistmaker
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 
 class SearchActivity : AppCompatActivity() {
+    companion object {
+        const val TEXT = "TEXT"
+    }
+
+    var input: String? = null
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(TEXT, input)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        input = savedInstanceState.getString(TEXT)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val inputText = findViewById<EditText>(R.id.editText)
+        inputText.setText(input)
+
+        val clearButton = findViewById<ImageView>(R.id.clearIcon)
+
+        clearButton.setOnClickListener {
+            inputText.setText("")
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            inputMethodManager?.hideSoftInputFromWindow(inputText.windowToken, 0)
+        }
+
+        val buttonBack = findViewById<TextView>(R.id.search_back)
+        buttonBack.setOnClickListener {
+            val backIntent = Intent(this, MainActivity::class.java)
+            startActivity(backIntent)
+        }
+
+
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                input = s.toString()
+            }
+        }
+        inputText.addTextChangedListener(textWatcher)
+    }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
     }
 }
--- app/src/main/java/com/e/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/e/playlistmaker/SettingsActivity.kt
@@ -1,11 +1,46 @@
 package com.e.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        val shareApp = findViewById<TextView>(R.id.share_app)
+        shareApp.setOnClickListener {
+            val shareAppIntent = Intent(Intent.ACTION_SEND)
+            shareAppIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.url_developer))
+            shareAppIntent.type = "*/*"
+            startActivity(shareAppIntent)
+        }
+
+        val writeInSupport = findViewById<TextView>(R.id.write_in_support)
+        writeInSupport.setOnClickListener {
+            val writeInSupportIntent = Intent(Intent.ACTION_SENDTO)
+            writeInSupportIntent.data = Uri.parse("mailto:")
+            writeInSupportIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.my_email)))
+            writeInSupportIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.mail_subject))
+            writeInSupportIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.mail_message))
+            startActivity(writeInSupportIntent)
+        }
+
+        val userAgreement = findViewById<TextView>(R.id.user_agreement)
+        userAgreement.setOnClickListener {
+            val userAgreementIntent =
+                Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.url_offer)))
+            startActivity(userAgreementIntent)
+        }
+
+        val buttonBack = findViewById<TextView>(R.id.settings_back)
+        buttonBack.setOnClickListener {
+            val backIntent = Intent(this, MainActivity::class.java)
+            startActivity(backIntent)
+        }
     }
 }
